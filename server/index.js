bfa8a6fc659282109d5f10fbb2b5724063346018
magdalenabegic <mabegic1@vernnet.hr>
2024-03-25 11:27:38 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 5500 });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (data) => {
+    console.log("Received: ", data);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
